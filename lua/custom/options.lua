df9b9ac7c9db0b78cfbe29cd1781da43df16353f
ianie <2817181586@qq.com>
2023-07-14 22:08:59 +0800

    rainbow outline

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,6 +1,6 @@
 lvim.transparent_window = true
 
-vim.api.nvim_command "autocmd InsertLeave,CursorMoved * normal! zz"
+vim.api.nvim_command("autocmd InsertLeave,CursorMoved * normal! zz")
 
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
@@ -10,16 +10,16 @@ vim.opt.softtabstop = 4
 vim.opt.cursorline = false
 
 if vim.fn.has("wsl") == 1 then
-    vim.g.clipboard = {
-        name = "win32yank-wsl",
-        copy = {
-            ["+"] = "win32yank.exe -i --crlf",
-            ["*"] = "win32yank.exe -i --crlf",
-        },
-        paste = {
-            ["+"] = "win32yank.exe -o --lf",
-            ["*"] = "win32yank.exe -o --lf",
-        },
-        cache_enabled = 0,
-    }
+	vim.g.clipboard = {
+		name = "win32yank-wsl",
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
 end
--- lua/custom/overrides.lua
+++ lua/custom/overrides.lua
@@ -1,61 +1,61 @@
 -- lvim.builtin.lualine.style = "default"
 
 lvim.lsp.on_attach_callback = function(client, bufnr)
-    require("lsp_signature").on_attach()
+	require("lsp_signature").on_attach()
 end
 
 lvim.builtin.treesitter = {
-    match = { enable = true },
-    textobjects = {
-        select = {
-            enable = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["id"] = "@conditional.inner",
-                ["ad"] = "@conditional.outer",
-            },
-        },
-        move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-                ["]["] = "@function.outer",
-                ["]m"] = "@class.outer",
-                ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-            },
-            goto_next_end = {
-                ["]]"] = "@function.outer",
-                ["]M"] = "@class.outer",
-            },
-            goto_previous_start = {
-                ["[["] = "@function.outer",
-                ["[m"] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[]"] = "@function.outer",
-                ["[M"] = "@class.outer",
-            },
-            goto_next = {
-                ["]d"] = "@conditional.outer",
-            },
-            goto_previous = {
-                ["[d"] = "@conditional.outer",
-            },
-        },
-    },
-    rainbow = {
-        enable = true,
-        colors = {
-            "#EC7063",
-            "#00BCD4",
-            "#F9BF3B",
-            "#87D37C",
-            "#AF7AC5",
-            "#3498DB",
-            "#FF9800",
-        },
-    },
+	match = { enable = true },
+	textobjects = {
+		select = {
+			enable = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+				["id"] = "@conditional.inner",
+				["ad"] = "@conditional.outer",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]["] = "@function.outer",
+				["]m"] = "@class.outer",
+				["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+			},
+			goto_next_end = {
+				["]]"] = "@function.outer",
+				["]M"] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[["] = "@function.outer",
+				["[m"] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[]"] = "@function.outer",
+				["[M"] = "@class.outer",
+			},
+			goto_next = {
+				["]d"] = "@conditional.outer",
+			},
+			goto_previous = {
+				["[d"] = "@conditional.outer",
+			},
+		},
+	},
+	rainbow = {
+		enable = true,
+		hlgroups = {
+			"DevIconCss",
+			"DevIconPyo",
+			"DevIconTxt",
+			"DevIconFsi",
+			"DevIconMl",
+			"DevIconSln",
+			"DevIconPpt",
+		},
+	},
 }
