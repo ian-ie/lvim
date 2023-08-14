793014bc3b0aed430f5b5e52af98c2cb9656e123
ianie <2817181586@qq.com>
2023-08-14 17:39:25 +0800

    del neorg

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -10,26 +10,6 @@ lvim.plugins = {
 			require("better_escape").setup()
 		end,
 	},
-	{
-		"nvim-neorg/neorg",
-		build = ":Neorg sync-parsers",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {}, -- Loads default behaviour
-					["core.concealer"] = {}, -- Adds pretty icons to your documents
-					["core.dirman"] = { -- Manages Neorg workspaces
-						config = {
-							workspaces = {
-								notes = "~/notes",
-							},
-						},
-					},
-				},
-			})
-		end,
-	},
 	{ "metakirby5/codi.vim", cmd = { "Codi", "CodiNew" } },
 	-- 高亮todo
 	{
