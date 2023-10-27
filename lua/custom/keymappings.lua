bd72c8fee060bb43ee49341fd34e0a5005b55738
ianie <2817181586@qq.com>
2023-10-27 11:14:27 +0800

    buffpick

--- lua/custom/keymappings.lua
+++ lua/custom/keymappings.lua
@@ -82,8 +82,11 @@ setwksn("n", "Notice")
 setwksn("r", "SnipRun")
 setwksn("t", "Trouble")
 setwks("s", { { "P", "<cmd>Telescope projects<cr>", "projects" } })
-setwks("l", { { "vo", ":lua vim.diagnostic.config({ virtual_text = true})<CR>", "open virtual" } })
-setwks("l", { { "vc", ":lua vim.diagnostic.config({ virtual_text = false})<CR>", "close virtual" } })
+setwks("b", { { "f", ":BufferLineCyclePick<cr>", "pick buffer" } })
+setwks("l", {
+	{ "vo", ":lua vim.diagnostic.config({ virtual_text = true})<CR>", "open virtual" },
+	{ "vc", ":lua vim.diagnostic.config({ virtual_text = false})<CR>", "close virtual" },
+})
 
 lvim.lsp.buffer_mappings.normal_mode["[e"] = {
 	function()
